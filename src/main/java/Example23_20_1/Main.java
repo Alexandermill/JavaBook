172a837bd7e280facf9c565e4183bb4fde5dccab
Avantesb <102500952+Avantesb@users.noreply.github.com>
2023-04-17 11:59:37 +0700

    SeekebleFileChannel testing

--- src/main/java/Example23_20_1/Main.java
+++ src/main/java/Example23_20_1/Main.java
@@ -1,15 +1,82 @@
 package Example23_20_1;
 
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Path path = Path.of("C:\\Users\\user\\Desktop\\JavaBook\\files\\input.txt");
+        Path path = Path.of("files\\input.txt");
 
-        readFixCharInLines(path, 20);
+        // readFixCharInLines(path, 20);
+
+        seekableReader(path, 80);
+
+
+    }
+
+    static void seekableReader(Path path, int charInLine) throws IOException{
+        SeekableByteChannel sbc = Files.newByteChannel(path, StandardOpenOption.READ);
+        ByteBuffer bufer = ByteBuffer.allocate(charInLine);
+
+        int i = 0;
+        int count = 0;
+        boolean flag = true;
+        String line = " ";
+        String temp ="";
+        String t2 = "";
+        String lineSepBySpace;
+        long skip = 0;
+
+        while (flag){
+            sbc.position(skip);
+            // System.out.print(skip + " ");
+            // System.out.println(count);
+
+            if((i = sbc.read(bufer)) < charInLine){
+                ByteBuffer bufer2 = ByteBuffer.allocate(i);
+                sbc.position(skip);
+                sbc.read(bufer2);
+                line = new String(bufer2.array());
+                lineSepBySpace = line;
+                flag = false;
+            } else {
+                temp = new String(bufer.array());
+                line = temp;
+
+                if(temp.contains("\n")){
+                    System.out.print("WARNING!!! this line contains \\n: ");
+                    
+                }
+                
+                lineSepBySpace = line.substring(0, line.lastIndexOf(" "));
+            }
+
+                      
+
+            if(lineSepBySpace.substring(0, 1).equals(" ")){
+                lineSepBySpace = lineSepBySpace.substring(1);
+                skip += 1;
+            }
+
+
+
+            
+            System.out.println(lineSepBySpace);
+
+
+            skip += Long.valueOf(lineSepBySpace.length());
+            bufer.clear();
+            count++;
+
+        }
+
+
+
 
 
     }
