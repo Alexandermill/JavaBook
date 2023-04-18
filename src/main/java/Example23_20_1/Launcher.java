8345ceed65d3d3677c4423e52c83ab739034ab11
Avantesb <102500952+Avantesb@users.noreply.github.com>
2023-04-18 17:44:37 +0700

    test concurrency 2

--- src/main/java/Example23_20_1/Launcher.java
+++ src/main/java/Example23_20_1/Launcher.java
@@ -6,9 +6,9 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 /*
@@ -40,8 +40,11 @@ public class Launcher {
 
     public static void main(String[] args) {
 
-        AtomicLong cursor = new AtomicLong();
+        AtomicLong cursor = new AtomicLong(0);
         BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(1024);
+
+        new Thread(new Reader(blockingQueue, cursor, "files\\input.txt")).start();
+        new Thread(new Parser(blockingQueue, cursor, 80)).start();
 
 
     }
@@ -49,13 +52,40 @@ public class Launcher {
     public static class Parser implements Runnable {
 
         BlockingQueue<String> blockingQueue = null;
-        long startPoint;
-        int buffer;
+        AtomicLong cursor = null;
+        int chunkSize;
 
+        public Parser(BlockingQueue<String> blockingQueue, AtomicLong cursor, int chunkSize) {
+            this.blockingQueue = blockingQueue;
+            this.cursor = cursor;
+            this.chunkSize = chunkSize;
+        }
 
         @Override
         public void run() {
-            System.out.println("Parser начал работу.");
+            System.out.println("Parser начал работу." +Thread.currentThread().getName());
+            while (true){
+                String line = null;
+                String temp;
+                try {
+                    line = blockingQueue.take();
+                    temp = line.substring(0, chunkSize);
+                    temp = temp.substring(0, temp.lastIndexOf(" "));
+
+                    System.out.println("Parser update cursor from " + cursor + " ");
+                    cursor.addAndGet(Long.valueOf(temp.length()));
+                    System.out.print("on" + cursor + " \n");
+                    System.out.println(temp);
+
+
+                    if(line.equals("EOF")){
+                        break;
+                    }
+
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
 
         }
 
@@ -64,27 +94,47 @@ public class Launcher {
     public static class Reader implements Runnable {
 
         BlockingQueue<String> blockingQueue = null;
-        int chunkSize;
         AtomicLong cursor = null;
         String path;
 
+        public Reader(BlockingQueue<String> blockingQueue, AtomicLong cursor, String path) {
+            this.blockingQueue = blockingQueue;
+            this.cursor = cursor;
+            this.path = path;
+        }
 
         @Override
         public void run() {
 
-            System.out.println("LineReader начал работу");
+            System.out.println("Reader начал работу в потоке "+Thread.currentThread().getName());
             SeekableByteChannel sbc = null;
             try {
-                sbc = Files.newByteChannel(Paths.get(path));
+                sbc = Files.newByteChannel(Paths.get(path), StandardOpenOption.READ);
 
-                ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                ByteBuffer byteBuffer = ByteBuffer.allocate(100);
+                int buferSize=0;
+                long oldCursor = 0;
 
-                while (sbc.read(byteBuffer) < 0) {
+                while (true) {
+
+                    if (oldCursor == cursor.get()){
+                        System.out.println("Cursor not updated " + oldCursor);
+                    }
+                    System.out.println("start on cursor "+cursor);
                     sbc.position(cursor.get());
+                    buferSize = sbc.read(byteBuffer);
                     blockingQueue.put(new String(byteBuffer.array()));
+//                    System.out.println(new String(byteBuffer.array()));
+                    oldCursor = cursor.get();
+
                     byteBuffer.clear();
-                }
-                blockingQueue.put("EOF");
+                        if(buferSize < 0){
+                            blockingQueue.put("EOF");
+                            break;
+                        }
+
+                    }
+
 
             } catch (IOException e) {
                 throw new RuntimeException(e);
